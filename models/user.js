0af226edb61b268394341cc2c2fc3af2f0cb29ce
Milton Chithenga <88409286+MarcuzAC@users.noreply.github.com>
2026-02-25 10:31:19 +0200

    User model

--- models/user.js
+++ models/user.js
@@ -40,9 +40,12 @@ const userSchema = new mongoose.Schema({
   }
 });
 
-// Hash password before saving
+// Hash password before saving - FIXED VERSION
 userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
+  // Only hash if password is modified or new
+  if (!this.isModified('password')) {
+    return next();
+  }
   
   try {
     const salt = await bcrypt.genSalt(10);
